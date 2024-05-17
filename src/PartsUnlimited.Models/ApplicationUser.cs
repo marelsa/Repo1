b4a0ff1ac6f6f319159e3d45a86f379cc6475c5c
marelsa <marelsa.lupe@gmail.com>
2024-05-17 10:27:53 -0500

    error local

--- src/PartsUnlimited.Models/ApplicationUser.cs
+++ src/PartsUnlimited.Models/ApplicationUser.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// error
 using Microsoft.AspNetCore.Identity;
 
 namespace PartsUnlimited.Models
