fa265c6b47cbd9482b6823b19b2ff2c0b340e84b
marelsa <marelsa.lupe@gmail.com>
2024-05-17 13:57:25 -0500

    Update CartItem.cs

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+//Cambio en Hib
 using System;
 using System.ComponentModel.DataAnnotations;
 //Mi primer commit//
